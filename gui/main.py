0c64686a166b820a83f08197cf07e17378a5ff14
David <hej>
2019-09-03 23:29:35 +0200

    One cell input that could be added to given cells

--- gui/main.py
+++ gui/main.py
@@ -1,78 +1,101 @@
 #!/c/Users/David/AppData/Local/Programs/Python/Python37-32/python
 import string
 import tkinter as tk
+from tkinter.font import Font
 
 from nattsud.solver import Solver
 
 CELL_SIZE = 30
 COLOR = "white"
 
+cells = dict()
+solver = Solver()
+
 
 def main():
-    main_window = tk.Tk()
-    main_window.title("nattsud - a sudoku helper")
-    main_window.geometry('300x300')
-    main_window.minsize(CELL_SIZE*14, CELL_SIZE*14)
+    main_window = create_main_window()
 
     c = tk.Canvas(main_window, bg=COLOR, height=250, width=100)
-
     c.place(width=CELL_SIZE*14, height=CELL_SIZE*14)
 
+    global start_button
     start_button = tk.Button(c, text="Set cells\n(1st: givens)", command=set_start)
-    calc_button = tk.Button(c, text="Calculate all possible\nvalues (simple)", command=calculate)
-    reduce_button = tk.Button(c, text="Reduce all possible\nvalues", command=reduce)
+    calc_button = tk.Button(c, text="Calculate all possible\nvalues (simple)", command=calculate_possible_numbers)
+    reduce_button = tk.Button(c, text="Reduce all possible\nvalues", command=reduce_possible_numbers)
     c.create_window(CELL_SIZE*0.5, CELL_SIZE*11, window=start_button, anchor=tk.NW)
     c.create_window(CELL_SIZE*3, CELL_SIZE*11, window=calc_button, anchor=tk.NW)
     c.create_window(CELL_SIZE*7.1, CELL_SIZE*11, window=reduce_button, anchor=tk.NW)
 
-    _draw_xy_labels_on_canvas(c)
+    draw_xy_labels_on_canvas(c)
+    draw_lines_on_canvas(c)
 
-    _draw_lines_on_canvas(c)
+    cells['a', 1] = tk.IntVar()
+    put_input_fields_on_canvas(c)
 
     print(main_window.size(), main_window.geometry())
 
     main_window.mainloop()
 
 
+def create_main_window():
+    main_window = tk.Tk()
+    main_window.title("nattsud - a sudoku helper")
+    main_window.geometry('300x300')
+    main_window.minsize(CELL_SIZE * 14, CELL_SIZE * 14)
+    return main_window
+
+
 def set_start():
     print("START!")
-    solver = Solver
-    solver.set_givens()
+    solver.set_givens(cells)
+    start_button['state'] = tk.DISABLED
 
 
-def calculate():
+def calculate_possible_numbers():
     print("CALC!")
 
 
-def reduce():
+def reduce_possible_numbers():
     print("REDUCE!")
 
 
-def _draw_xy_labels_on_canvas(c):
+def draw_xy_labels_on_canvas(c):
     grid_x = 0
     grid_y = 0
     for x in list(string.ascii_lowercase[:9]):
         x_label = tk.Label(c, text=x, bg=COLOR)
         grid_x += 1
-        c.create_window(CELL_SIZE * grid_x, 0, window=x_label, anchor=tk.NW)
+        c.create_window(CELL_SIZE*(grid_x+0.5), 0, window=x_label, anchor=tk.N)
     for y in list(range(1, 10)):
         y_label = tk.Label(c, text=y, bg=COLOR)
         grid_y += 1
-        c.create_window(0, CELL_SIZE * grid_y, window=y_label, anchor=tk.NW)
+        c.create_window(CELL_SIZE/2, CELL_SIZE*(grid_y+0.5), window=y_label, anchor=tk.E)
 
 
-def _draw_lines_on_canvas(c):
+def draw_lines_on_canvas(c):
     grid_end = CELL_SIZE * 10
     for line_nr in list(range(0, 4)):
         region_end = CELL_SIZE + CELL_SIZE*line_nr*3
-        c.create_line(0, region_end, grid_end, region_end)  # horizontal
-        c.create_line(region_end, 0, region_end, grid_end)  # vertical
+        c.create_line(CELL_SIZE, region_end, grid_end, region_end)  # horizontal
+        c.create_line(region_end, CELL_SIZE, region_end, grid_end)  # vertical
     for line_nr in list(range(0, 3)):
         for region_line_nr in list(range(1, 3)):
             cell_end = CELL_SIZE + CELL_SIZE*line_nr*3 + CELL_SIZE*region_line_nr
             print(cell_end/CELL_SIZE)
             c.create_line(CELL_SIZE, cell_end, grid_end, cell_end, dash=(3,3))  # horizontal
             c.create_line(cell_end, CELL_SIZE, cell_end, grid_end, dash=(3,3))  # vertical
+
+
+def put_input_fields_on_canvas(c):
+    cells['a', 1] = tk.Spinbox(c,
+                               width=1,
+                               font=Font(size=int(CELL_SIZE/2)),
+                               values=("", 1, 2, 3, 4, 5, 6, 7, 8, 9),
+                               wrap=True,
+                               state='readonly',
+                               readonlybackground=COLOR,
+                               disabledbackground="light grey")
+    c.create_window(CELL_SIZE, CELL_SIZE, window=cells['a', 1], anchor=tk.NW)
 
 
 def _idx(y):
--- nattsud/solver.py
+++ nattsud/solver.py
@@ -1,8 +1,23 @@
 #!/c/Users/David/AppData/Local/Programs/Python/Python37-32/python
 
+import tkinter as tk
 
 class Solver:
 
-    @staticmethod
-    def set_givens():
+    def __init__(self):
+        self.given_cells = {}
+        self.set_cells = {}
+
+    def set_givens(self, cells):
         print("GIVENS")
+        for cell in cells:
+            print(cell, ":", cells[cell].get())
+            if cells[cell].get() is not "":
+                self.given_cells[cell] = cells[cell].get()
+                cells[cell]['state'] = tk.DISABLED
+                # cells[cell].configure(bg = "blue")
+                print("value:", cells[cell].get())
+        print("Given cells:")
+        for given_cell in self.given_cells:
+            print(given_cell, "", self.given_cells[cell])
+            # given_cell['state'] = tk.DISABLED
